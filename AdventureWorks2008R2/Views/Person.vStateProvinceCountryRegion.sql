50f64dc24ca1001931bda867359966c45bc8cd55
ZMU <econtrol@mail.ru>
2015-07-20 18:17:15 +0300

    -

---
+++ AdventureWorks2008R2/Views/Person.vStateProvinceCountryRegion.sql
@@ -0,0 +1,52 @@
+
+CREATE VIEW [Person].[vStateProvinceCountryRegion] 
+WITH SCHEMABINDING 
+AS 
+SELECT 
+    sp.[StateProvinceID] 
+    ,sp.[StateProvinceCode] 
+    ,sp.[IsOnlyStateProvinceFlag] 
+    ,sp.[Name] AS [StateProvinceName] 
+    ,sp.[TerritoryID] 
+    ,cr.[CountryRegionCode] 
+    ,cr.[Name] AS [CountryRegionName]
+FROM [Person].[StateProvince] sp 
+    INNER JOIN [Person].[CountryRegion] cr 
+    ON sp.[CountryRegionCode] = cr.[CountryRegionCode];
+
+GO
+
+SET ARITHABORT ON
+GO
+
+SET CONCAT_NULL_YIELDS_NULL ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+SET ANSI_WARNINGS ON
+GO
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [IX_vStateProvinceCountryRegion] ON [Person].[vStateProvinceCountryRegion] 
+(
+	[StateProvinceID] ASC,
+	[CountryRegionCode] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Clustered index on the view vStateProvinceCountryRegion.' , @level0type=N'SCHEMA',@level0name=N'Person', @level1type=N'VIEW',@level1name=N'vStateProvinceCountryRegion', @level2type=N'INDEX',@level2name=N'IX_vStateProvinceCountryRegion'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Joins StateProvince table with CountryRegion table.' , @level0type=N'SCHEMA',@level0name=N'Person', @level1type=N'VIEW',@level1name=N'vStateProvinceCountryRegion'
+GO
+
---
+++ AdventureWorks2008R2/Views/dbo.tstobjs.sql
@@ -0,0 +1,4 @@
+CREATE VIEW tstobjs AS 
+SELECT s.id, s.xtype, s.uid, s.info, s.replinfo FROM sysobjects AS s
+GO
+
