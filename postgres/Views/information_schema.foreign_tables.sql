807b6223b282156519b8ecfddba63c313ea4abc5
ZMU <econtrol@mail.ru>
2015-09-17 16:09:03 +0300

    -

--- postgres/Views/information_schema.foreign_tables.sql
+++ postgres/Views/information_schema.foreign_tables.sql
@@ -1,7 +1,22 @@
-CREATE VIEW "information_schema"."foreign_tables" AS
+SET SCHEMA "information_schema";
+
+CREATE VIEW foreign_tables AS
  SELECT _pg_foreign_tables.foreign_table_catalog,
     _pg_foreign_tables.foreign_table_schema,
     _pg_foreign_tables.foreign_table_name,
     _pg_foreign_tables.foreign_server_catalog,
     _pg_foreign_tables.foreign_server_name
-   FROM information_schema._pg_foreign_tables;
+   FROM _pg_foreign_tables;
+
+
+ALTER TABLE information_schema.foreign_tables OWNER TO postgres;
+
+--
+-- Name: foreign_tables; Type: ACL; Schema: information_schema; Owner: postgres
+--
+
+REVOKE ALL ON TABLE foreign_tables FROM PUBLIC;
+REVOKE ALL ON TABLE foreign_tables FROM postgres;
+GRANT ALL ON TABLE foreign_tables TO postgres;
+GRANT SELECT ON TABLE foreign_tables TO PUBLIC;
+
