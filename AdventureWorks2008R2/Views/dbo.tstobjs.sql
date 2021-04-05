c10c968b162fb1aadc6ddf7f8b869080bc9c1547
MUZakharov <econtrol@mail.ru>
2021-04-05 14:08:27 +0300

    -

--- AdventureWorks2008R2/Views/dbo.tstobjs.sql
+++ AdventureWorks2008R2/Views/dbo.tstobjs.sql
@@ -1,4 +1,14 @@
+/************************************************************
+ * Code formatted by SoftTree SQL Assistant © v12.0.52
+ * Time: 05.04.2021 14:08:14
+ ************************************************************/
+
 CREATE VIEW tstobjs AS 
-SELECT s.id, s.xtype, s.uid, s.info, s.replinfo FROM sysobjects AS s
+SELECT s.id,
+       s.xtype,
+       s.uid,
+       s.info,
+       s.replinfo
+FROM   sysobjects AS s
 GO
 -- comment
